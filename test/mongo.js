58dc226900f6c4335cc59b941d353021fea83e27
fxy27 <fxy27@ccs.neu.edu>
2017-03-21 17:05:41 -0400

    build server

---
+++ test/mongo.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+var testSchema = mongoose.Schema({
+    test: String
+})
+
+var testModel = mongoose.model('Test', testSchema);
+
+var testModel = mongoose.model("TestModel", TestSchema);
+
+testModel.findAllMessages = findAllMessages;
+testModel.createMessage = createMessage;
+testModel.deleteMessage = deleteMessage;
+
+module.exports = testModel;
+
+function findAllMessages(req, res) {
+    TestModel
+        .find()
+        .then(
+            function(tests) {
+                res.json(tests);
+            },
+            function(err) {
+                res.status(400).send(err);
+            }
+        );
+}
+
+function createMessage(req, res) {
+    TestModel
+        .create(req.body)
+        .then(
+            function(test) {
+                res.json(test);
+            },
+            function(err) {
+                res.status(400).send(err);
+            }
+        );
+}
+
+function deleteMessage(req, res) {
+    TestModel
+        .remove({_id: req.params.id})
+        .then(
+            function(result) {
+                res.json(result);
+            },
+            function(err) {
+                res.status(400).send(err);
+            }
+        );
+}
